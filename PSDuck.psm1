c65caf2250a0175092cd7cf90d5e03a3d1204547
Christian Lujan <clujan@spartans.ut.edu>
2014-10-25 23:01:42 -0400

    first commit

---
+++ PSDuck.psm1
@@ -0,0 +1,28 @@
+﻿
+
+Function Confirm-Has_Interface{
+  param([parameter(Mandatory=$true)]$InputObject,[parameter(Mandatory=$true)][System.String[]]$Contract)
+      
+      $obj = @{} 
+      $obj.MeetsRequirements = $false
+             
+      switch($InputObject){         
+      { $_ -is [System.Collections.Hashtable]} {$this = $_ 
+                                                $obj.MissingValues = $Contract| %{ if( -not $this.ContainsKey($_) ) {$_}  }                                              
+                                                break
+                                                }
+      { $_ -is [PSObject]}    { $this = $_ 
+                                $properties = $this|Get-Member|Select-Object -ExpandProperty Name
+                                $obj.MissingValues = $Contract| %{ if( -not $properties.Contains($_) ) {$_}  } 
+                                break
+                              }     
+      default {throw "-InputObject for 'Assert-Missing_Property function' must by an object or hashtable"}
+      } #end switch
+
+     
+      if( $obj.MissingValues -eq $null){
+         $obj.MeetsRequirements = $true
+      }
+    
+      $obj
+}  #End Assert-Has_Interface
---
+++ unit_tests/PSDuck.Test.ps1
@@ -0,0 +1,122 @@
+﻿Import-Module PSate
+Import-Module PShould
+
+$parentFolder = Split-Path -Path $PSScriptRoot -Parent
+Import-Module (Join-Path -Path $parentFolder -ChildPath PSDuck.psm1)
+
+Describing 'PSDuck.ps1'{
+    TestSetup{
+      $required_properties = @('Property1','Property2','Property3','Property4')
+    
+    }
+
+    Given 'a PSObject with required properties'{
+  
+        TestSetup{
+              
+              $testObject = New-Object PSObject -Property @{Property1='';Property2='';Property3='';Property4=''}
+              $interface = Confirm-Has_Interface -InputObject $testObject -Contract $required_properties
+    
+        }
+
+        It 'will return true'{
+          $interface.MeetsRequirements | should be $true
+    
+        }
+
+        It 'will have no missing properties'{
+          $interface.MissingValues.Count | should be -eq 0
+          $interface.MissingValues | should be $null
+        }
+  
+    }  #End Given
+
+    Given 'a PSObject missing one required property'{
+  
+        TestSetup{
+              #$required_properties = @('Property1','Property2')
+              $testObject = New-Object PSObject -Property @{Property1='';Property3='';Property4=''}
+              $interface = Confirm-Has_Interface -InputObject $testObject -Contract $required_properties
+    
+        }
+
+        It 'will return false'{
+          $interface.MeetsRequirements | should be $false
+    
+        }
+
+        It 'will list one missing property'{
+          $interface.MissingValues.Count | should be -eq 1
+          $interface.MissingValues | should be 'Property2'
+        }
+
+       
+  
+    }  #End Given
+
+    Given 'a hashtable with required properties'{
+  
+        TestSetup{             
+              $testHashTable =  @{Property1='';Property2='';Property3='';Property4=''}
+              $interface = Confirm-Has_Interface -InputObject $testHashTable -Contract $required_properties
+    
+        }
+
+        It 'will return true'{
+          $interface.MeetsRequirements | should be $true
+    
+        }
+
+        It 'will have no missing properties'{
+          $interface.MissingValues.Count | should be -eq 0
+          $interface.MissingValues | should be $null
+        }
+
+    }  #End Given
+
+    Given 'a hashtable without keys'{
+  
+        TestSetup{              
+              $testHashTable = @{}
+              $interface = Confirm-Has_Interface -InputObject $testHashTable -Contract $required_properties
+    
+        }
+
+        It 'will return false'{
+          $interface.MeetsRequirements | should be $false
+    
+        }
+
+        It 'will list four missing properties'{
+          $interface.MissingValues.Count | should be -eq 4
+          $interface.MissingValues | should be $required_properties
+        }
+    }  #End Given
+
+    Given 'a hashtable missing two required properties'{
+  
+        TestSetup{
+              #$required_properties = @('Property1','Property2')
+              $testHashTable = @{Property1='';Property4=''}
+              $interface = Confirm-Has_Interface -InputObject $testHashTable -Contract $required_properties
+    
+        }
+
+        It 'will return false'{
+          $interface.MeetsRequirements | should be $false
+    
+        }
+
+        It 'will list two  missing properties'{
+          $interface.MissingValues.Count | should be -eq 2
+          $interface.MissingValues | should be 'Property2','Property3'
+    
+        }
+  
+    }  #End Given
+
+
+}  #End Describing
+
+Remove-Module PSDuck
+
