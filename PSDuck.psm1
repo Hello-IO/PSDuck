3aeada8a4ee1fa08a11c1bc91d5ebd5edfd670bb
Christian Lujan <clujan@spartans.ut.edu>
2014-10-26 12:46:28 -0400

    updated hash method to include members and keys

--- PSDuck.psm1
+++ PSDuck.psm1
@@ -1,23 +1,23 @@
 ﻿
 
 Function Confirm-Has_Members{
-  param([parameter(Mandatory=$true)]$InputObject,[parameter(Mandatory=$true)][System.String[]]$Members)
+  param([parameter(Mandatory=$true)]$InputObject,[parameter(Mandatory=$true)][System.String[]]$ExpectedMembers)
       
       $obj = @{} 
       $obj.MeetsRequirements = $false
-             
+      $actualMembers      
       switch($InputObject){         
-      { $_ -is [System.Collections.Hashtable]} {$this = $_ 
-                                                $obj.MissingValues = $Members| %{ if( -not $this.ContainsKey($_) ) {$_}  }                                              
-                                                break
+      { $_ -is [System.Collections.Hashtable]} { #Collect hashtable object members including keys.
+                                                 $actualMembers = ($_|Get-Member|Select-Object -ExpandProperty Name) + ($_|Select-Object -ExpandProperty Keys)                                           
+                                                 break
                                                 }           
-      default  { $this = $_ 
-                                $properties = $this|Get-Member|Select-Object -ExpandProperty Name
-                                $obj.MissingValues = $Members| %{ if( -not $properties.Contains($_) ) {$_}  } 
-                                break
-                              }
+      default  { #Collect object members.
+                 $actualMembers = $_|Get-Member|Select-Object -ExpandProperty Name
+                 break
+                }
       } #end switch
 
+      $obj.MissingValues = $ExpectedMembers| %{ if( -not $actualMembers.Contains($_) ) {$_}  }
      
       if( $obj.MissingValues -eq $null){
          $obj.MeetsRequirements = $true
